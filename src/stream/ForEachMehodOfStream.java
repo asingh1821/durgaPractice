33d28445ce52a994a74966ca91706affc422223e
asingh1821 <kkamit777@gmail.com>
2025-12-23 00:29:20 +0530

    next commit

--- src/stream/ForEachMehodOfStream.java
+++ src/stream/ForEachMehodOfStream.java
@@ -5,9 +5,9 @@ import java.util.stream.Stream;
 public class ForEachMehodOfStream {
     public static void main(String[] args) {
         Stream<Integer> s = Stream.of(9,99,999,9999,99999);
-        s.forEach(System.out::println);
-
+        s.forEach(System.out::print);
+        System.out.print(">>>>>>>>>>>>>>>>>>>>>");
         Integer[] i = {10,20,30,40,50,60};
-        Stream.of(i).forEach(System.out::println);
+        Stream.of(i).forEach(System.out::print);
     }
 }
