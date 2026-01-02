178db30c3a4a96e36a551d82eeab8e2cdf079484
asingh1821 <kkamit777@gmail.com>
2026-01-02 23:29:04 +0530

    example2 commit

--- src/serializationAndDeserializationWithoutJacksonLib/example2/Solution.java
+++ src/serializationAndDeserializationWithoutJacksonLib/example2/Solution.java
@@ -1,2 +1,19 @@
-package serializationAndDeserializationWithoutJacksonLib.example1.example2;public class Solution {
+package serializationAndDeserializationWithoutJacksonLib.example2;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Solution {
+    public static void main(String[] args) {
+        Student st = new Student("Amit", 28);
+        String fileName = "Gipgr.txt";
+        try{
+            FileOutputStream fos = new FileOutputStream(fileName);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            fos.close();
+        }catch(IOException e){
+
+        }
+    }
 }
