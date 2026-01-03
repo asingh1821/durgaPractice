72885c4959107d4217f95328b4a152e8c36ac26f
asingh1821 <kkamit777@gmail.com>
2026-01-03 19:41:30 +0530

    second transient and static commit

--- src/serializationAndDeserializationWithoutJacksonLib/exampleUsingTransientStatic/Sol.java
+++ src/serializationAndDeserializationWithoutJacksonLib/exampleUsingTransientStatic/Sol.java
@@ -1,2 +1,45 @@
-package serializationAndDeserializationWithoutJacksonLib.exampleUsingTransientStatic;public class Sol {
+package serializationAndDeserializationWithoutJacksonLib.exampleUsingTransientStatic;
+
+import java.io.*;
+
+public class Sol {
+    public static void main(String[] args) {
+        School school = new School("JNV", 350, 20, 25);
+        printData(school);
+        String fileName = "School.txt";
+
+//      serialization
+        try{
+            FileOutputStream fos = new FileOutputStream(fileName);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(school);
+            oos.close();
+            fos.close();
+            System.out.println("Serialization completed....");
+//            printData(school);
+        }catch(IOException e){
+            System.out.println("exception caught....");
+        }
+
+//        Deserialization
+        try{
+            FileInputStream fis = new FileInputStream(fileName);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            School sch = (School) ois.readObject();
+            ois.close();
+            fis.close();
+            System.out.println("Deserilaztion completed...");
+            printData(sch);
+        }catch(IOException e){
+            System.out.println("exception caught1111....");
+        }catch(ClassNotFoundException e1){
+            System.out.println("class not found exception ");
+        }
+    }
+    public static void printData(School sc) {
+        System.out.println("name: "+ sc.name);
+        System.out.println("no. of Students: " + sc.students);
+        System.out.println("no. of rooms: " + sc.rooms);
+        System.out.println("no. of teachers: "+ School.teacher);
+    }
 }
