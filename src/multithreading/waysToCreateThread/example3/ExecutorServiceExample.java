6b032bc80fdbdaa207a6a1b452f7a56b63307e63
asingh1821 <kkamit777@gmail.com>
2026-01-14 22:32:53 +0530

    executor commit

--- src/multithreading/waysToCreateThread/example3/ExecutorServiceExample.java
+++ src/multithreading/waysToCreateThread/example3/ExecutorServiceExample.java
@@ -1,2 +1,16 @@
-package multithreading.waysToCreateThread.example3;public class ExecutorServiceExample {
+package multithreading.waysToCreateThread.example3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(()->
+                System.out.println("Test 1 executing"));
+        executor.submit(()->
+                System.out.println("Test 2 executing"));
+
+        executor.shutdown();
+    }
 }
