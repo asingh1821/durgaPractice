74250aa885a4b0932fb1ffb2fd0d8d9a88529169
asingh1821 <kkamit777@gmail.com>
2026-02-09 22:33:11 +0530

    des commit

--- src/serializationAndDeserializationWithoutJacksonLib/example1/Employee.java
+++ src/serializationAndDeserializationWithoutJacksonLib/example1/Employee.java
@@ -36,4 +36,13 @@ public class Employee implements Serializable {
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
 }
