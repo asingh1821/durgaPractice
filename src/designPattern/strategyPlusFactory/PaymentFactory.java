88d4d37bab2940a2a69fada21a4d7d0587261c84
asingh1821 <kkamit777@gmail.com>
2026-02-10 23:24:00 +0530

    strtgy commit

--- src/designPattern/strategyPlusFactory/PaymentFactory.java
+++ src/designPattern/strategyPlusFactory/PaymentFactory.java
@@ -1,2 +1,12 @@
-package designPattern.strategyPlusFactory;public class PaymentFactory {
+package designPattern.strategyPlusFactory;
+
+class PaymentFactory {
+    public static PaymentService paymentfactory(String input){
+        return switch (input) {
+            case "Net" -> new NetBanking();
+            case "Card" -> new CreditCardPayment();
+            case "UPI" -> new UPIPayment();
+            default -> throw new IllegalArgumentException("wrong input taken");
+        };
+    }
 }
