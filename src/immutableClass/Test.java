a84e2cd6cd2e026ff37e4c1d0626a232973b920e
asingh1821 <kkamit777@gmail.com>
2026-02-12 23:38:39 +0530

    immutable commit

--- src/immutableClass/Test.java
+++ src/immutableClass/Test.java
@@ -1,2 +1,11 @@
-package immutableClass;public class Test {
+package immutableClass;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        User u = new User(1, Arrays.asList("HR","Dev","Manager"));
+        u.getRoles().forEach(System.out::println);
+    }
 }
