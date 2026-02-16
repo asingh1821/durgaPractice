88f0ac5854d6ecf366f525a98debfbf4bd66b746
asingh1821 <kkamit777@gmail.com>
2026-02-16 21:19:51 +0530

    hash commit

--- src/hashCodeAndEqualsContract/example4/Test.java
+++ src/hashCodeAndEqualsContract/example4/Test.java
@@ -1,2 +1,25 @@
-package hashCodeAndEqualsContract.example4;public class Test {
+package hashCodeAndEqualsContract.example4;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Test {
+    public static void main(String[] args) {
+        Employee e1 = new Employee(1,"Amit");
+        Employee e2 = new Employee(2,"Saurav");
+        Employee e3 = new Employee(3, "Loli");
+        Employee e4 = new Employee(4,"Sonma");
+        Employee e5 = new Employee(1, "Amit");
+        HashSet<Employee> set = new HashSet<>();
+        set.add(e1);
+        set.add(e2);
+        set.add(e3);
+        set.add(e4);
+        set.add(e5);
+        Iterator<Employee> it = set.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next().toString());
+        }
+
+    }
 }
