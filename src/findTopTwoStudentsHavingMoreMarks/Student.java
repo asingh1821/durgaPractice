93e15af6c19784c71baeb60f0c36fee20d9af24c
asingh1821 <kkamit777@gmail.com>
2026-02-24 22:52:52 +0530

    topper commit

--- src/findTopTwoStudentsHavingMoreMarks/Student.java
+++ src/findTopTwoStudentsHavingMoreMarks/Student.java
@@ -1,2 +1,67 @@
-package findTopTwoStudentsHavingMoreMarks;public class Student {
+package findTopTwoStudentsHavingMoreMarks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private int roll;
+    List<Subject> sub;
+
+    public Student(String name, int roll, List<Subject> sub) {
+        this.name = name;
+        this.roll = roll;
+        this.sub = sub;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public List<Subject> getSub() {
+        return sub;
+    }
+
+    public void setSub(List<Subject> sub) {
+        this.sub = sub;
+    }
+
+    public int getTotalMarks(){
+        int sum = 0;
+        if(this.sub == null) {
+            return sum;
+        }
+        for(Subject subject : this.sub) {
+            sum += subject.getMarks();
+        }
+        return sum;
+    }
+    public List<Subject> getSortedSubjectByMarks(){
+        List<Subject> sortedSubject = new ArrayList<>();
+        if(sub == null) {
+            return sortedSubject;
+        }
+        sortedSubject.addAll(this.sub);
+        Collections.sort(sortedSubject, new Comparator<Subject>() {
+            @Override
+            public int compare(Subject s1, Subject s2) {
+                return (int) (s2.getMarks()-s1.getMarks());
+            }
+        });
+        return sortedSubject;
+    }
 }
---
+++ src/hotelService/Hotel.java
@@ -0,0 +1,2 @@
+package hotelService;public class Hotel {
+}
