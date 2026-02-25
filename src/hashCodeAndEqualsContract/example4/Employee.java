7f00474e1803d478a099c46517426d2ecce65339
asingh1821 <kkamit777@gmail.com>
2026-02-25 23:35:19 +0530

    hsEq commit

--- src/hashCodeAndEqualsContract/example4/Employee.java
+++ src/hashCodeAndEqualsContract/example4/Employee.java
@@ -1,2 +1,52 @@
-package hashCodeAndEqualsContract.example4;public class Employee {
+package hashCodeAndEqualsContract.example4;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public boolean equals(Object o){
+        if(this == o) {
+            return true;
+        }
+        if(o == null || this.getClass() != o.getClass()){
+            return false;
+        }
+        Employee emp = (Employee) o;
+        return name.equals(emp.getName()) && id==emp.getId();
+    }
+//    @Override
+//    public int hashCode(){
+//        return Objects.hash(id,name);
+//    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
