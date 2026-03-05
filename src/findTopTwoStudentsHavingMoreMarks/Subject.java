cd6fab1bf33b8ed0be227cb658500069d92bfc25
asingh1821 <kkamit777@gmail.com>
2026-03-05 09:25:53 +0530

    more_marks commit

--- src/findTopTwoStudentsHavingMoreMarks/Subject.java
+++ src/findTopTwoStudentsHavingMoreMarks/Subject.java
@@ -1,2 +1,27 @@
-package findTopTwoStudentsHavingMoreMarks;public class Subject {
+package findTopTwoStudentsHavingMoreMarks;
+
+public class Subject {
+    private String subjectName;
+    private double marks;
+
+    public Subject(String subjectName, double marks) {
+        this.subjectName = subjectName;
+        this.marks = marks;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
 }
