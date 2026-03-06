d4622891f25e20faa0c6360950f388cc80bb4c0d
asingh1821 <kkamit777@gmail.com>
2026-03-06 23:16:58 +0530

    serialization_deserialization commit

--- src/serializationAndDeserializationWithoutJacksonLib/example3/Student.java
+++ src/serializationAndDeserializationWithoutJacksonLib/example3/Student.java
@@ -1,2 +1,17 @@
-package serializationAndDeserializationWithoutJacksonLib.example3;public class Student {
+package serializationAndDeserializationWithoutJacksonLib.example3;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int roll;
+
+    public Student(String name, int roll){
+        this.name = name;
+        this.roll = roll;
+    }
+    public String toString(){
+        return "Student name: "+ name+ " roll No.  "+ roll;
+    }
 }
+
--- src/serializationAndDeserializationWithoutJacksonLib/example3/Test.java
+++ src/serializationAndDeserializationWithoutJacksonLib/example3/Test.java
@@ -1,2 +1,41 @@
-package serializationAndDeserializationWithoutJacksonLib.example3;public class Test {
+package serializationAndDeserializationWithoutJacksonLib.example3;
+
+import serializationAndDeserializationWithoutJacksonLib.example1.Employee;
+
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student("Amit", 1);
+        String file = "filname.exe";
+        //Serialization
+        try{
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(s1);
+            fos.close();
+            oos.close();
+            System.out.println(s1.toString());
+            System.out.println("object serialized....");
+        }catch (IOException e){
+            System.out.println("exceptions handled");
+            e.printStackTrace();
+        }
+        // Deserialization
+        Student student = null;
+        try{
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            student = (Student) ois.readObject();
+            System.out.println("object is deserialized.......");
+            System.out.println(student.toString());
+            fis.close();
+            ois.close();
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("IOException is caught");
+        }catch (ClassNotFoundException e) {
+            System.out.println("class not found exceptions....");
+        }
+    }
 }
