60b55ed5d9882c2432c95a328e149e0c73f19409
asingh1821 <kkamit777@gmail.com>
2026-05-05 20:08:35 +0530

    hotel_service commit

--- src/hotelService/Test.java
+++ src/hotelService/Test.java
@@ -1,2 +1,20 @@
-package hotelService;public class Test {
+package hotelService;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Hotel h1 = new Hotel("CityInn", "London",120.0,
+                Arrays.asList("MONDAY","TUESDAY","WEDNESDAY"));
+        Hotel h2 = new Hotel("SeaView", "Brighton",150.0,
+                Arrays.asList("TUESDAY","WEDNESDAY"));
+        Hotel h3 = new Hotel("LakePoint", "London",130.0,
+                Arrays.asList("WEDNESDAY"));
+        List<Hotel> listOfHotel = Arrays.asList(h1,h2,h3);
+        Hotel h = new Hotel();
+        List<Hotel> sortedHotelList =  h.sortHotelByPrice(listOfHotel);
+        System.out.println(sortedHotelList);
+
+    }
 }
